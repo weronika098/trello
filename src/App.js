05c8134671969a9f9032151253c239b395173b4f
Weronika <w.zaporowicz@gmail.com>
2023-09-22 18:26:38 +0200

    podstawy aplikacji

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
+import React from "react";
 import Homepage from "./pages/Homepage";
+import Header from "./components/Header";
+import { DndProvider } from "react-dnd";
+import {HTML5Backend} from "react-dnd-html5-backend";
 
-function App() {
-  return (
-    <div className="App">
-      <Homepage />
-    </div>
-  );
-}
+const App = () => {
+    return (
+        <DndProvider backend={HTML5Backend}>
+            <Header />
+            <Homepage />
+        </DndProvider>
+    );
+};
 
 export default App;
--- src/components/Col.jsx
+++ src/components/Col.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Col = ({ isOver, children }) => {
-    const className = isOver ? "highlight-region" : "";
+    const className = isOver ? " highlight-region" : "";
 
     return (
         <div className={`col${className}`}>
